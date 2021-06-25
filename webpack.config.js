53dfc6b901afeb75e39920b35d6d35dffa38182c
Juan Velazquez <juandvelaz@gmail.com>
2021-06-24 21:42:19 -0500

    BE Update: Created server index.js file and configured Webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: ['./client/index.js'],
+  mode: 'development',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
