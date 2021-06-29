d73d74132cec92a0b22e7cd7dee54ef00d9d4142
Juan Velazquez <juandvelaz@gmail.com>
2021-06-29 16:49:38 -0500

    FE UPDATE: Set up React routing foundation

--- client/index.js
+++ client/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, NavBar } from './components';
 
-ReactDOM.render(<div>Hello, world!</div>, document.getElementById('index'));
+ReactDOM.render(
+  <Router>
+    <NavBar />
+    <Route exact path="/" component={Home} />
+  </Router>,
+  document.getElementById('index')
+);
