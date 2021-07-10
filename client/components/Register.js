b52bd41a25fedaad921b97e45d904607940672b4
Juan Velazquez <juandvelaz@gmail.com>
2021-07-10 15:23:49 -0500

    FE UPDATE: Created Register.js component along with its Redux reducer

--- client/components/Register.js
+++ client/components/Register.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { newUser } from '../store/User';
 
 export class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      userPassword: '',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.newUser({ ...this.state });
+    this.setState({ userName: '', userPassword: '' });
+  };
+
   render() {
+    const { userName, userPassword } = this.state;
+    const { handleSubmit, handleChange } = this;
+
     return (
       <div>
         <h1>Register</h1>
-        <form>
-          <h2>Username</h2>
-          <input />
-          <h2>Password</h2>
-          <input />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="userName">Username:</label>
+          <input name="userName" onChange={handleChange} value={userName} />
+          <label htmlFor="password">Password:</label>
+          <input name="password" onChange={handleChange} value={userPassword} />
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapDispatchToProps = (dispatch) => {
+  return { newUser: (user) => dispatch(newUser(user)) };
+};
 
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Register);
+export default connect(null, mapDispatchToProps)(Register);
--- client/store/User.js
+++ client/store/User.js
@@ -1,3 +1,30 @@
-export default (state = {}, action) => {
-  return state;
+import axios from 'axios';
+
+const NEW_USER = 'NEW_USER';
+
+const _newUser = (user) => ({
+  type: NEW_USER,
+  user,
+});
+
+export const newUser = (user) => {
+  return async (dispatch) => {
+    try {
+      const { data: createdUser } = await axios.post('/api/register', user);
+      dispatch(_newUser(createdUser));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+const initialState = {};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case NEW_USER:
+      return action.user;
+    default:
+      return state;
+  }
 };
